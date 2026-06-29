07eccf985a61c6b02f3014b2d45a9466740ac345
manjmangoo-spec <manjmangoo@gmail.com>
2026-06-29 01:04:10 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+      base: '/SITE-HeracTO/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
